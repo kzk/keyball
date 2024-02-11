4d9769d5c701c9ac360ce33cb20094df9bfcefd0
Kazuki Ohta <kazuki.ohta@gmail.com>
2024-02-11 00:59:50 -0800

    initial commit

---
+++ keyball44/keymaps/kzk9/config.h
@@ -0,0 +1,34 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define TAPPIN_TERM 150
+
+#define TAP_CODE_DELAY 5
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#define KEYBALL_CPI_DEFAULT 1100
+#define KEYBALL_SCROLL_DEV_DEFAULT 4
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_TIME 500
